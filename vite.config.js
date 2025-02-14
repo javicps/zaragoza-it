016a15511e56905f56d496a2b7ec5b336428fdde
javicps <javicps@gmail.com>
2025-02-14 23:06:48 +0100

    Fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-// https://vite.dev/config/
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-});
+  plugins: [react()],
+  base: '/zaragoza-it/',
+  test: {
+    globals: true, // Allows using global test functions like test, expect
+    environment: 'jsdom', // Simulates a browser-like environment for testing React components
+  },
+  resolve: {
+    alias: {
+      '@components': '/src/components',
+      '@styles': '/src/styles',
+    },
+  },
+})
