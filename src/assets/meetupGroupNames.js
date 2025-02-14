52765901d080a58270eadd25c9b11c6ba769fab5
javicps <javicps@gmail.com>
2025-02-14 15:31:47 +0100

    Add group names

--- src/assets/meetupGroupNames.js
+++ src/assets/meetupGroupNames.js
@@ -2,7 +2,7 @@ export const groupNames = [
   'devops-zaragoza',
   'agilearagon',
   'python_zgz',
-
+  'gdgzaragoza',
   'zaragoza-hackathons',
   'dotnetters',
   'meetup-group-docker-zaragoza',
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache } from '@apollo/client'
+const ACCESS_TOKEN =
+  'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJzdWIiOiIxODU5MDMzNDkiLCJuYmYiOjE3MzkxMTMzMjksInJvbGUiOiJmaXJzdF9wYXJ0eSIsImlzcyI6Ii5tZWV0dXAuY29tIiwicXVhbnR1bV9sZWFwZWQiOmZhbHNlLCJyZWZyZXNoX3Rva2Vuc19jaGFpbl9pZCI6Ijk2OGQ5OGQyLTZmYWEtNDk0YS1hNDgxLWY1NDdiZjExMzU0OCIsImV4cCI6MTczOTExNjkyOSwiaWF0IjoxNzM5MTEzMzI5LCJqdGkiOiIzOTRlMjlhYy1jMTFiLTRiMTItYWMxNi05ODJhMjc0NWU3ODYiLCJvYXV0aF9jbGllbnRfaWQiOiI4NnQzbnNtcWgzNGw5ZGw3MmFjMWt0dXVwaCJ9.GQXvnUepYkDB8aKhkzBNd0EuXcGwYyhSGP1-o2gUYZ5HeALSBYa9Lwdl1zFZ6wU5XMvgyNVGyv-Ydxg8MTw0hA'
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'http://localhost:3001/api/events',
+    uri: 'https://cors-anywhere.herokuapp.com/https://api.meetup.com/gql',
+    headers: {
+      Authorization: `Bearer ${ACCESS_TOKEN}`,
+      'Content-Type': 'application/json',
+    },
   }),
   cache: new InMemoryCache(),
 })
