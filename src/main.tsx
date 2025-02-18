22b3b7b1c04ab8de097aef76b4fbcc93459de1f0
javicps <javicps@gmail.com>
2025-02-18 11:59:50 +0100

    Change to hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache } from '@apollo/client'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, HashRouter, Route } from 'react-router-dom'
 import TechGroups from './components/TechGroups'
 const ACCESS_TOKEN =
   'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJzdWIiOiIxODU5MDMzNDkiLCJuYmYiOjE3MzkxMTMzMjksInJvbGUiOiJmaXJzdF9wYXJ0eSIsImlzcyI6Ii5tZWV0dXAuY29tIiwicXVhbnR1bV9sZWFwZWQiOmZhbHNlLCJyZWZyZXNoX3Rva2Vuc19jaGFpbl9pZCI6Ijk2OGQ5OGQyLTZmYWEtNDk0YS1hNDgxLWY1NDdiZjExMzU0OCIsImV4cCI6MTczOTExNjkyOSwiaWF0IjoxNzM5MTEzMzI5LCJqdGkiOiIzOTRlMjlhYy1jMTFiLTRiMTItYWMxNi05ODJhMjc0NWU3ODYiLCJvYXV0aF9jbGllbnRfaWQiOiI4NnQzbnNtcWgzNGw5ZGw3MmFjMWt0dXVwaCJ9.GQXvnUepYkDB8aKhkzBNd0EuXcGwYyhSGP1-o2gUYZ5HeALSBYa9Lwdl1zFZ6wU5XMvgyNVGyv-Ydxg8MTw0hA'
@@ -19,8 +19,8 @@ const client = new ApolloClient({
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ApolloProvider client={client}>
-    <BrowserRouter basename="/zaragoza-it">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </ApolloProvider>
 )
